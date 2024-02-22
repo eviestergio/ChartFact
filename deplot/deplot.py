d44834d31767a9c282834348608aff552f03e5a8
eviestergio <paraskevi.stergiopoulou@kcl.ac.uk>
2024-02-21 23:44:10 -0800

    Tailored DePlot script to local PNGs
    - TO-DO: update path for all pngs

--- deplot/deplot.py
+++ deplot/deplot.py
@@ -1,13 +1,22 @@
 from transformers import Pix2StructProcessor, Pix2StructForConditionalGeneration
-import requests
+# import requests
 from PIL import Image
+import glob
 
 processor = Pix2StructProcessor.from_pretrained('google/deplot')
 model = Pix2StructForConditionalGeneration.from_pretrained('google/deplot')
 
-url = "https://raw.githubusercontent.com/vis-nlp/ChartQA/main/ChartQA%20Dataset/val/png/5090.png"
-image = Image.open(requests.get(url, stream=True).raw)
+folder_path = '../seed_datasets/ChartQA/test/png/'
 
-inputs = processor(images=image, text="Generate underlying data table of the figure below:", return_tensors="pt")
-predictions = model.generate(**inputs, max_new_tokens=512)
-print(processor.decode(predictions[0], skip_special_tokens=True))
+png_files = glob.glob(f'{folder_path}*.png') #list of all pngs
+
+print(f"Number of PNG files found: {len(png_files)}") 
+
+for file_path in png_files:
+    image = Image.open(file_path)
+
+    inputs = processor(images=image, text="Generate underlying data table of the figure below:", return_tensors="pt")
+    predictions = model.generate(**inputs, max_new_tokens=512)
+    
+    decoded_predictions = processor.decode(predictions[0], skip_special_tokens=True)
+    print(f"Data for {file_path}:\n{decoded_predictions}\n")
