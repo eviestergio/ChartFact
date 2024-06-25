7b67c529818bbb58849e286f1df9beef98eec813
eviestergio <paraskevi.stergiopoulou@kcl.ac.uk>
2024-06-25 14:34:21 +0300

    Generate missing underlying tables for PNGs of 100 random samples

--- deplot/deplot.py
+++ deplot/deplot.py
@@ -6,7 +6,7 @@ import csv
 processor = Pix2StructProcessor.from_pretrained('google/deplot')
 model = Pix2StructForConditionalGeneration.from_pretrained('google/deplot')
 
-folder_path = '../seed_datasets'
+folder_path = '../random_sample_100'
 
 # Recursively iterate through all files and directories in folder 
 for root, dirs, files in os.walk(folder_path):
@@ -16,6 +16,18 @@ for root, dirs, files in os.walk(folder_path):
         if file_name.lower().endswith('.png'):
             # Construct the full path to the PNG file
             file_path = os.path.join(root, file_name)
+
+            # Determine the path for the corresponding CSV file
+            parent_dir = os.path.dirname(root)
+            # Create CSV directory and file paths relative to the parent directory
+            csv_directory = os.path.join(parent_dir, 'tables')
+            csv_file_path = os.path.join(csv_directory, file_name.replace('.png', '.csv'))
+            
+            # Check if the CSV file already exists
+            if os.path.exists(csv_file_path):
+                print(f"Skipping because CSV file already exists for {file_path}.")
+                continue
+
             # DePlot the PNG
             image = Image.open(file_path)
             inputs = processor(images=image, text="Generate underlying data table of the figure below:", return_tensors="pt")
@@ -23,18 +35,13 @@ for root, dirs, files in os.walk(folder_path):
             decoded_predictions = processor.decode(predictions[0], skip_special_tokens=True)
             print(f"Data for {file_path}:\n{decoded_predictions}\n")
 
-            # Extract the dataset name from the file path
-            dataset_name = root.split(os.path.sep)[-1]
-
-            parent_dir = os.path.dirname(root)
-            # Create CSV directory and file paths relative to the parent directory
-            csv_directory = os.path.join(parent_dir, 'tables')
-            csv_file_path = os.path.join(csv_directory, file_name.replace('.png', '.csv'))
-            os.makedirs(csv_directory, exist_ok=True) #create CSV directory if it doesn't exist
+            # Create CSV directory if it doesn't exist
+            os.makedirs(csv_directory, exist_ok=True) 
             
             # Write prediction to CSV file in respective folder
             with open(csv_file_path, 'w', newline='') as csv_file:
                 writer = csv.writer(csv_file)
                 writer.writerow([decoded_predictions])
+            print(f"CSV file saved for {file_path}")
 
 print("CSV files saved successfully.")
