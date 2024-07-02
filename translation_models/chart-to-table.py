39fdab81f55605e08684aa1cc32ee5ccb1ea084a
eviestergio <paraskevi.stergiopoulou@kcl.ac.uk>
2024-07-02 14:25:54 +0300

    Update chart-to-table script to save outputs properly

--- translation_models/chart-to-table.py
+++ translation_models/chart-to-table.py
@@ -7,14 +7,21 @@ import os
 
 # Check for CREATE GPU availability and set as device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Using device {device}")
 
 model_name = 'khhuang/chart-to-table'
 model = VisionEncoderDecoderModel.from_pretrained(model_name).to(device) # move to GPU
 processor = DonutProcessor.from_pretrained(model_name)
+print("Model and processor loaded successfully")
 
 # Paths to FigureQA and PlotQA datasets
-figureqa_pattern = '../random_sample_100-2/FigureQA/**/*.png'
-plotqa_pattern = '../random_sample_100-2/PlotQA/**/*.png'
+# figureqa_pattern = '../random_sample_100-2/FigureQA/**/*.png'
+# plotqa_pattern = '../random_sample_100-2/PlotQA/**/*.png'
+figureqa_pattern = os.path.expanduser('~/seed_datasets-100-2/FigureQA/**/*.png')
+plotqa_pattern = os.path.expanduser('~/seed_datasets-100-2/PlotQA/**/*.png')
+
+print(f"FigureQA pattern: {figureqa_pattern}")
+print(f"PlotQA pattern: {plotqa_pattern}")
 
 # Combine the patterns to match all csv files in FigureQA and PlotQA
 patterns = [figureqa_pattern, plotqa_pattern]
@@ -24,10 +31,11 @@ for pattern in patterns:
     for file_path in glob.glob(pattern, recursive=True):
         # Construct the full path to the PNG file
         parent_dir = os.path.dirname(file_path)
+        grandparent_dir = os.path.dirname(parent_dir) # parent dir of png folder
         file_name = os.path.basename(file_path)
             
         # Determine the path for the corresponding CSV file
-        csv_directory = os.path.join(parent_dir, 'tables')
+        csv_directory = os.path.join(grandparent_dir, 'tables')
         base_name = os.path.splitext(file_name)[0]
         csv_file_path = os.path.join(csv_directory, f"{base_name}-ctt.csv")
         
